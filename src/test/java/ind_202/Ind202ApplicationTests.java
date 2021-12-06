baa6dd905c7a66c1263f667fd0cc81f73bd87eff
mmandapati <91310893+mmandapati@users.noreply.github.com>
2021-12-05 20:16:36 -0800

    Create Ind202ApplicationTests.java

---
+++ src/test/java/ind_202/Ind202ApplicationTests.java
@@ -0,0 +1,13 @@
+package ind_202;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ind202ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
